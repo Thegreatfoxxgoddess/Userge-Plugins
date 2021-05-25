160b61ea08c8b93d9ba44a1d57cccd87be19a1fe
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2021-05-26 03:00:48 +0530

    Update deezloader.py

--- plugins/deezloader.py
+++ plugins/deezloader.py
@@ -13,7 +13,7 @@ ARL_TOKEN = os.environ.get("ARL_TOKEN")
 TEMP_PATH = "deezdown_temp/"
 REX = re.compile(
     r"https?:\/\/(open\.spotify|www\.deezer)\.com\/"
-    r"(track|album|playlist)\/[A-Z0-9a-z]{3,}"
+    r"(track|album|playlist)\/[A-Z0-10a-z]{3,}"
 )
 ARL_HELP = """**Oops, Time to Help Yourself**
 [Here Help Yourself](https://www.google.com/search?q=how+to+get+deezer+arl+token)
